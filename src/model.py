62e6c13a7e8f36040ee14249af81abc4a1a4c468
Rocky Tapader <rocky@Rockys-MacBook-Air.local>
2025-10-26 14:26:02 +0530

    model added

--- src/model.py
+++ src/model.py
@@ -4,13 +4,14 @@ import numpy as np
 import pandas as pd
 import os
 
+
 # Load data
-xtrain = np.loadtxt(
-    '/Users/rocky/Desktop/MLops/DVC/Employee Salary Prediction/data/raw/train/xtrain.csv',
-    delimiter=' ',  # Use ',' if CSV is comma-separated
+xtrain = np.genfromtxt(
+    '/Users/rocky/Desktop/MLops/Employee Salary Prediction/salary_prediction/data/raw/xtrain.txt',
+    delimiter=','
 )
 ytrain = pd.read_csv(
-    '/Users/rocky/Desktop/MLops/DVC/Employee Salary Prediction/data/raw/train/ytrain.csv'
+    '/Users/rocky/Desktop/MLops/Employee Salary Prediction/salary_prediction/data/raw/ytrain.csv'
 ).to_numpy()
 
 # Train model
@@ -18,8 +19,8 @@ model = LinearRegression()
 model.fit(xtrain, ytrain)
 
 # Save model
-save_folder = '/Users/rocky/Desktop/MLops/DVC/Employee Salary Prediction/models'
-os.makedirs(save_folder, exist_ok=True)  # create folder only
+save_folder = '/Users/rocky/Desktop/MLops/Employee Salary Prediction/salary_prediction/models'
+os.makedirs(save_folder, exist_ok=True)
 
 save_file = 'linear_model.pkl'
 save_path = os.path.join(save_folder, save_file)
@@ -28,4 +29,5 @@ with open(save_path, 'wb') as f:
     pickle.dump(model, f)
 
 print(f"Model saved at {save_path}")
+ 
 
